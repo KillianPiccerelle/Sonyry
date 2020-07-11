a58da8bceba1e25b303abc09ade3f9548bb3fc0e
MartineauAn <martineau.antoine2001@gmail.com>
2020-07-12 01:44:06 +0200

    Redisign des pages

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -4,28 +4,40 @@
 @section('content')
 
     <div class="container">
-
+        <div class="card-header text-center">
+            <h1>Liste de mes pages :</h1>
+        </div>
+        <br>
         <div class="row">
-            @foreach($pages as $page)
-                <div class="col-md-4">
-                    <div class="bi-text text-center">
-                        <h5>{{ $page->title }}</h5>
-                        <small>Dernière modification : {{ $page->updated_at }}</small>
+            @if(count($pages) > 0)
+                @foreach($pages as $page)
+                    <div class="col-md-4">
+                        <div class="card text-center">
+                            <div class="card-header">
+                                <h5><span>{{ $page->title }}</span></h5>
+                            </div>
+                            <div class="card-body">
+                                <div>
+                                    <img src="/storage/pages/{{ $page->user_id }}/{{ $page->image }}">
+                                </div>
+                                <br>
+                                <div class="text-center">
+                                    <a href="#" class="btn btn-primary">
+                                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                                        Editer
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <small>Dernière modification : {{ $page->updated_at }}</small>
+                            </div>
+                        </div>
+                        <br>
                     </div>
-                    <br>
-                    <div class="text-center">
-                        <img src="/storage/pages/{{ $page->user_id }}/{{ $page->image }}">
-                    </div>
-
-                    <br>
-                    <div class="text-center">
-                        <a href="#" class="btn btn-primary">
-                            <i class="fa fa-pencil" aria-hidden="true"></i>
-                             Editer
-                        </a>
-                    </div>
-                </div>
-            @endforeach
+                @endforeach
+            @else
+                <p>Pas de pages.</p>
+            @endif
         </div>
     </div>
 
