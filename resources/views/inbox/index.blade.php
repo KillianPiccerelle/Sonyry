9a83149463a365c4eea4aa1be17453ee93942cf0
Killian P <KillianPiccerelle@sfr.fr>
2020-08-06 15:19:56 +0200

    inbox work

--- resources/views/inbox/index.blade.php
+++ resources/views/inbox/index.blade.php
@@ -12,7 +12,7 @@
                 <div class="row align-items-stretch">
                     <!--nav-->
                     <div class="sidebar d-none d-lg-block col-md-2 pt-3 bg-white">
-                        <ul class="nav nav-tabs" id="navTab" role="tablist">
+                        <ul class="nav nav-tabs" id="navTabPrimary" role="tablist">
                             <li class="nav-item" role="presentation">
                                 <a class="nav-link active" id="inbox-tab" data-toggle="tab" href="#inbox" role="tab"
                                    aria-controls="inbox" aria-selected="true"><i class="fa fa-envelope"></i> Boîte de
@@ -20,7 +20,7 @@
                             </li>
                             <li class="nav-item" role="presentation">
                                 <a class="nav-link" id="favourite-tab" data-toggle="tab" href="#favourite" role="tab"
-                                   aria-controls="favorite" aria-selected="false"><i class="fa fa-star"></i> Favoris</a>
+                                   aria-controls="favourite" aria-selected="false"><i class="fa fa-star"></i> Favoris</a>
                             </li>
                             <li class="nav-item" role="presentation">
                                 <a class="nav-link" id="trash-tab" data-toggle="tab" href="#trash" role="tab"
@@ -30,66 +30,47 @@
                     </div>
                     <!--List notification-->
                     <div class="notification-list-container col-md-3 pt-4 pb-4 border-right bg-white">
-                        <!--Nav INBOX-->
-                        <div class="tab-content" id="tabNotification">
-                            <div class="tab-pane fade show active" id="inbox" role="tabpanel"
-                                 aria-labelledby="inbox-tab">
 
-                                <ul class="nav flex-column nav-tabs" id="myTab" role="tablist">
-                                    @php
-                                    $i = 0;
+                        <div class="tab-content" id="navTabPrimaryContent">
+                            <!--Nav INBOX-->
+                            <div class="tab-pane fade show active" id="inbox" role="tabpanel" aria-labelledby="inbox-tab">
 
-                                    @endphp
+                                <ul class="nav nav-tabs flex-column" id="navTabSecondary" role="tablist">
+                                    <li class="nav-item" role="presentation">
+                                        <a class="nav-link active" id="welcome-tab" data-toggle="tab" href="#welcome" role="tab" aria-controls="welcome" aria-selected="true">
+                                            <p class="sender-name">Système</p> Bienvenue
+                                        </a>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <a class="nav-link" id="welcome2-tab" data-toggle="tab" href="#welcome2" role="tab" aria-controls="welcome2" aria-selected="true">
+                                            <p class="sender-name">Système</p> Bienvenue2
+                                        </a>
+                                    </li>
+                                    @php $id = 0 @endphp
                                     @foreach($inboxes as $inbox)
-                                        <li class="nav-item" role="presentation">
-                                            <a class="nav-link @if($i === 0) active @endif" id="home-tab" data-toggle="tab" href="#{{ $inbox->notification->id }}"
-                                               role="tab" aria-controls="home"
-                                               aria-selected="true"> <p class="sender-name"> Système </p>
-                                                {{ $inbox->notification->title }}</a>
-                                        </li>
-                                        @php $i++ @endphp
+                                    <li class="nav-item" role="presentation">
+                                        <a class="nav-link" id="id{{$id}}-tab" data-toggle="tab" href="#id{{$id}}" role="tab" aria-controls="id{{$id}}" aria-selected="true">
+                                            <p class="sender-name">Système</p> {{ $inbox->notification->title }}
+                                        </a>
+                                    </li>
+                                        @php $id++ @endphp
                                     @endforeach
                                 </ul>
 
                             </div>
-                            <div class="tab-pane fade show " id="favourite" role="tabpanel"
-                                 aria-labelledby="favourite-tab">
-                                <!--Nav FAVORITE-->
-                                @foreach($inboxes as $inbox)
-                                    <div class="tab-pane fade" id="favorite" role="tabpanel"
-                                         aria-labelledby="favorite-tab">
-                                        <!-- nav for notification FAVOURITE-->
-                                        <ul class="nav nav-tabs" id="favouriteContentTab" role="tablist">
-                                            <li class="nav-item" role="presentation">
-                                                <a class="nav-link active" id="favourite-notification-tab"
-                                                   data-toggle="tab" href="#"
-                                                   role="tab"
-                                                   aria-controls="FavouriteContent"
-                                                   aria-selected="true">{{ $inbox->notification->paragraph }}</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                @endforeach
+
+                            <!--Nav FAVOURITE-->
+                            <div class="tab-pane fade" id="favourite" role="tabpanel" aria-labelledby="favourite-tab">
+                                Test 2
                             </div>
-                            <div class="tab-pane fade show " id="trash" role="tabpanel" aria-labelledby="trash-tab">
-                                <!--NAV TRASH-->
-                                @foreach($inboxes as $inbox)
-                                    <div class="tab-pane fade" id="trash" role="tabpanel" aria-labelledby="trash-tab">
-                                        <!-- nav for notification TRASH-->
-                                        <ul class="nav nav-tabs" id="trashContentTab" role="tablist">
-                                            <li class="nav-item" role="presentation">
-                                                <a class="nav-link active" id="trash-notification-tab" data-toggle="tab"
-                                                   href="#"
-                                                   role="tab"
-                                                   aria-controls="trashContent"
-                                                   aria-selected="true">{{ $inbox->notification->created_at }}</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                @endforeach
+
+                            <!--Nav TRASH-->
+                            <div class="tab-pane fade" id="trash" role="tabpanel" aria-labelledby="trash-tab">
+                                Test 3
                             </div>
                         </div>
                     </div>
+
                     <!--Body notification-->
                     <div class="notification-view d-none d-md-block col-md-9 col-lg-7 bg-white">
                         <div class="row">
@@ -102,32 +83,62 @@
                             </div>
                         </div>
 
-                            <div class="tab-content" id="myTabContent">
-                                @php
-                                    $i = 0;
-                                @endphp
-                                @foreach($inboxes as $inbox)
-                                    <div class="tab-pane fade show @if($i === 0) active @endif" id="{{ $inbox->notification->id }}" role="tabpanel"
-                                         aria-labelledby="{{ $inbox->notification->id }}-tab">
-                                        <div class="notification-body">
-                                            <div class="sender-details">
-                                                <img class="img-sm rounded-circle mr-3" width="100" height="100"
-                                                     src="/img/avatar.default.jpg" alt="">
-                                                <div class="details">
-                                                    <p class="msg-subject">{{ $inbox->notification->title }} {{ $inbox->notification->created_at }}</p>
-                                                    <p class="sender-notification">Système</p>
-                                                </div>
-                                            </div>
-                                            <div class="message-content">
-                                                <p>{{ $inbox->notification->paragraphe }}</p>
-                                            </div>
+                        <div class="tab-content" id="navTabSecondaryContent">
+                            <div class="tab-pane fade show active" id="welcome" role="tabpanel" aria-labelledby="welcome-tab">
+                                <div class="notification-body">
+                                    <div class="sender-details">
+                                        <img class="img-sm rounded-circle mr-3" width="100" height="100"
+                                             src="/img/avatar.default.jpg" alt="">
+                                        <div class="details">
+                                            <p class="msg-subject">Bienvenue</p>
+                                            <p class="sender-notification">Système</p>
                                         </div>
                                     </div>
-                                    @php $i++ @endphp
-                                @endforeach
+                                    <div class="message-content">
+                                        <p>Bonjour, merci à vous de nous avoir rejoins</p>
+                                    </div>
+                                </div>
                             </div>
+                            <div class="tab-pane fade" id="welcome2" role="tabpanel" aria-labelledby="welcome2-tab">
+                                <div class="notification-body">
+                                    <div class="sender-details">
+                                        <img class="img-sm rounded-circle mr-3" width="100" height="100"
+                                             src="/img/avatar.default.jpg" alt="">
+                                        <div class="details">
+                                            <p class="msg-subject">Bienvenue2</p>
+                                            <p class="sender-notification">Système2</p>
+                                        </div>
+                                    </div>
+                                    <div class="message-content">
+                                        <p>Bonjour, merci à vous de nous avoir rejoins2</p>
+                                    </div>
+                                </div>
+                            </div>
+                            @php $id = 0 @endphp
+                            @foreach($inboxes as $inbox)
+                            <div class="tab-pane fade" id="id{{$id}}" role="tabpanel" aria-labelledby="id{{$id}}-tab">
+                                <div class="notification-body">
+                                    <div class="sender-details">
+                                        <img class="img-sm rounded-circle mr-3" width="100" height="100"
+                                             src="/img/avatar.default.jpg" alt="">
+                                        <div class="details">
+                                            <p class="msg-subject">{{ $inbox->notification->title }} {{ $inbox->notification->created_at }}</p>
+                                            <p class="sender-notification">Système</p>
+                                        </div>
+                                    </div>
+                                    <div class="message-content">
+                                        <p>{{ $inbox->notification->paragraph }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                                @php $id++ @endphp
+                            @endforeach
+                        </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
 
 
 
