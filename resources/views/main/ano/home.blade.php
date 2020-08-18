902b642c72c88db9a1ffb29f89e91f8c45c99cb8
Samy <samangelo24@gmail.com>
2020-08-18 18:49:28 +0200

    loginFront update

--- resources/views/main/ano/home.blade.php
+++ resources/views/main/ano/home.blade.php
@@ -2,23 +2,22 @@
 
 @section('content')
 
-    <div class="background"></div>
 
-    <div style="margin-top: 120px;" id="carouselExemple" class="carousel slide" data-ride="carousel" data-interval="5000">
+
+    <div id="option" class="carousel slide  carousel-fade" data-pause="hover" data-interval="3000">
 
         <ol class="carousel-indicators">
-            <li data-target="#carouselExemple" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExemple" data-slide-to="1"></li>
-            <li data-target="#carouselExemple" data-slide-to="2"></li>
+            <li data-target="#option" data-slide-to="0" class=""></li>
+            <li data-target="#option" data-slide-to="1"></li>
+            <li data-target="#option" data-slide-to="2"></li>
         </ol>
 
 
         <div class="carousel-inner">
 
             <div class="carousel-item active">
-                <img
-                    src="/img/carou2.jpg"
-                    class="d-block w-100">
+                <img src="/img/carousel1.jpg"
+                     class="d-block w-100">
                 <div class="carousel-caption d-none d-md-block">
                     <h1>Créez</h1>
                     <p>Créez votre portefolio.</p>
@@ -27,7 +26,7 @@
 
             <div class="carousel-item">
                 <img
-                    src="/img/carou1.jpg"
+                    src="/img/carousel2.jpg"
                     class="d-block w-100">
                 <div class="carousel-caption d-none d-md-block">
                     <h1>Collaborez</h1>
@@ -37,7 +36,7 @@
 
             <div class="carousel-item">
                 <img
-                    src="/img/carou3.jpg"
+                    src="/img/carousel3.jpg"
                     class="d-block w-100">
                 <div class="carousel-caption d-none d-md-block">
                     <h1>Partagez</h1>
@@ -47,25 +46,23 @@
 
         </div>
 
-        <a   class="carousel-control-prev" href="#carouselExemple" role="button" data-slide="prev">
+        <a href="#option" class="carousel-control-prev" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Précédent</span>
+            <span class="sr-only">Previous</span>
         </a>
-        <a  class="carousel-control-next" href="#carouselExemple" role="button" data-slide="next">
+        <a href="#option" class="carousel-control-next" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Suivant</span>
+            <span class="sr-only">Next</span>
         </a>
 
 
-    </div>
+    </div><br>
+
+    <footer id="sticky-footer" class="py-1  pb-0.5 bg-theme"><div class="container text-center"><small style="color: white; padding-bottom: -0.5rem">Copyright 2020 © Sonyry</small></div></footer>
 
 
     <script>
-        $('.carousel').carousel({
 
-            pause: "null"
-
-        })
     </script>
 
 @endsection
