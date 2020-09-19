04330807e6b713d691d9a590ec228967c119ffcb
Killian Piccerelle <KillianPiccerelle@sfr.fr>
2020-09-19 10:30:05 +0200

    test

--- resources/views/ancienhomeplusutilite.blade.php
+++ resources/views/ancienhomeplusutilite.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Vous êtes connecté') }}
                 </div>
             </div>
         </div>
